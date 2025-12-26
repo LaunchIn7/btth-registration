cf839a5fbdfd222398576d0c871ac0f939f6da5c
shabh2412 <rishabhpanesar@gmail.com>
2025-12-26 16:46:58 +0530

    Add Meta Pixel tracking code to root layout for Facebook conversion tracking
    - Import Next.js Script component for optimized third-party script loading
    - Add Meta Pixel initialization script with ID 3220546341440227 using afterInteractive strategy
    - Include PageView event tracking on all pages
    - Add noscript fallback image for users with JavaScript disabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
+import Script from 'next/script'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { SiteHeader } from '@/components/site-header'
@@ -37,6 +38,30 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* Meta Pixel Code */}
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3220546341440227');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                "<img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=3220546341440227&ev=PageView&noscript=1' />",
+            }}
+          />
+          {/* End Meta Pixel Code */}
+        </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f7f8fc] text-[#1c2238]`}>
           <SiteHeader />
           {children}
